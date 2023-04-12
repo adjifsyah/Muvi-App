db647775d704474a2b1bc2aa80634dfbf58a0270
adjifsyah <adji.firma>
2023-04-12 16:11:03 +0700

    Add Project

---
+++ Muvi-App/Muvi-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muvi-App
+//
+//  Created by Adji Firmansyah on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
